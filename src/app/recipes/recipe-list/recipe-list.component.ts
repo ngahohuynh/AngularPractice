28a2dfde016b85292e148ab2c310dd2630246a66
Ho Huynh Nga <hnga3511@gmail.com>
2020-05-14 15:08:55 +0700

    Add basic components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Hamburger', 'Hamburger Recipe', 'http://icons.iconarchive.com/icons/pixelkit/tasty-bites/256/hamburger-icon.png'),
+    new Recipe('Sandwich', 'Sandwich', 'http://icons.iconarchive.com/icons/google/noto-emoji-food-drink/256/32386-sandwich-icon.png'),
+    new Recipe('Omelette', 'Omelette Recipe', 'http://icons.iconarchive.com/icons/graphicloads/food-drink/256/egg-2-icon.png'),
+    new Recipe('Steak', 'Steak Recipe', 'http://icons.iconarchive.com/icons/aha-soft/desktop-buffet/256/Steak-icon.png')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Beef', 2),
+    new Ingredient('Lettuce', 5),
+    new Ingredient('Tomato', 4),
+    new Ingredient('Bread', 2)
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
